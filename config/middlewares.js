8e4880f5843ebd26489e8cb68a0d65f386beffa9
Appnovate <appnovate789@gmail.com>
2024-02-17 10:48:35 +0530

    fix:cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::cors',
+  // {
+  //   name: 'strapi::cors',
+  //   config: {
+  //     origin: 'http://silly-nightingale.142-93-209-108.plesk.page',
+  //     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  //     headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  //     keepHeaderOnError: true,
+  //   },
+  // },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
