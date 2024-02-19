a950c230ebbdd9f464a3c1420fca36601ba987a3
Appnovate <appnovate789@gmail.com>
2024-02-19 12:33:59 +0530

    fix:cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,16 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
-  // {
-  //   name: 'strapi::cors',
-  //   config: {
-  //     origin: 'https://vibrant-wu.172-104-186-52.plesk.page',
-  //     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-  //     headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-  //     keepHeaderOnError: true,
-  //   },
-  // },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      header: '*',
+      origin: ['http://localhost','http://localhost:1337','https://vibrant-wu.172-104-186-52.plesk.page']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
